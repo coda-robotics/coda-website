6c7dfa7dca0038521ece1e74c488c10239eb862b
vxnuaj <jv.100420@gmail.com>
2025-05-24 14:33:50 -0400

    update website head title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        <title>CODA</title>
         <link rel="icon" href="/codaheadlogo.png" type="image/png" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
